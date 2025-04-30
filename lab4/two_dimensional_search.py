7a4d8b157f492ac32b0676e75b95d1e49cb33cd9
NCCMNT <chmielewski2003@gmail.com>
2025-04-30 23:01:30 +0200

    lab5

--- lab4/two_dimensional_search.py
+++ lab4/two_dimensional_search.py
@@ -12,7 +12,18 @@ def find_pattern_in_column(text_column: str, pattern_columns: list[str]) -> list
     # TODO: Zaimplementuj wyszukiwanie kolumn wzorca w kolumnie tekstu
     # TODO: Dla każdej kolumny wzorca, przeszukaj kolumnę tekstu
     # TODO: Zwróć listę krotek (pozycja, indeks kolumny) dla znalezionych dopasowań
-    return []
+    n = len(text_column)
+    if n == 0 or len(pattern_columns) == 0: return []
+    results = []
+    
+    for j, pcol in enumerate(pattern_columns):
+        m = len(pcol)
+        if m > n: continue
+        for i in range(n - m + 1):
+            if pcol == text_column[i:i+m]:
+                results.append((i, j))
+
+    return results
 
 
 def find_pattern_2d(text: list[str], pattern: list[str]) -> list[tuple[int, int]]:
@@ -31,4 +42,31 @@ def find_pattern_2d(text: list[str], pattern: list[str]) -> list[tuple[int, int]
     # TODO: Sprawdź, czy wszystkie wiersze mają taką samą długość
     # TODO: Zaimplementuj algorytm wyszukiwania dwuwymiarowego
     # TODO: Zwróć listę współrzędnych lewego górnego rogu dopasowanego wzorca
-    return []
+    a, b = len(text), len(pattern)
+    if a == 0 or b == 0: return []
+
+    c, d = len(text[0]), len(pattern[0])
+    if d > c or b > a: return []
+
+    if any(len(line) != d for line in pattern) or any(len(line) != c for line in text): return []
+    results = []
+    pattern_cols = list(''.join(x) for x in zip(*pattern))
+    S = set()
+
+    for i in range(c):
+        text_col = ''.join([text[j][i] for j in range(a)])
+        found = find_pattern_in_column(text_col, pattern_cols)
+        for row, num in found:
+            S.add((row, i, num))
+
+    for i in range(a):
+        for j in range(c - d + 1):
+            flag = True
+            for m in range(d):
+                if (i,j+m,m) not in S:
+                    flag = False
+                    break
+            if flag:
+                results.append((i,j))
+
+    return results

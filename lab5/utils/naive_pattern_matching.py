f11705761daefcf6a56aaeaaa76a583b42978e61
NCCMNT <chmielewski2003@gmail.com>
2025-05-24 22:33:27 +0200

    lab5 plotting

--- lab5/utils/naive_pattern_matching.py
+++ lab5/utils/naive_pattern_matching.py
@@ -16,13 +16,24 @@ def naive_pattern_match(text: str, pattern: str) -> list[int]:
     # 3. If all characters match, add the starting position to the results
     # 4. Handle edge cases like empty patterns and patterns longer than the text
     if pattern == "": return []
+
+    def compare(str1, str2):
+        if len(str1) != len(str2): return False, 0
+        compares = 0
+        for i in range(len(str1)):
+            compares += 1
+            if str1[i] != str2[i]:
+                return False, compares
+        return True, compares
+
     n = len(text)
     m = len(pattern)
     result = []
     compares = 0
-    for i in range(n - m + 1):
-        if text[i:i+m] == pattern:
+    for i in range(n):
+        comp_result, add_to_compares = compare(text[i:i+m], pattern)
+        if comp_result:
             result.append(i)
-        compares += m
+        compares += add_to_compares
 
     return result, compares
--- lab5/utils/rabin_karp_algorithm.py
+++ lab5/utils/rabin_karp_algorithm.py
@@ -18,38 +18,49 @@ def rabin_karp_pattern_match(text: str, pattern: str, prime: int = 101) -> tuple
     # 4. Use rolling hash to efficiently compute hash values of text windows
     # 5. Return all positions where the pattern is found in the text
     # Note: Use the provided prime parameter for the hash function to avoid collisions
-    BASE = 256
 
-    def hash(s: str):
-        n = len(s)
-        h = 0
-        for i, c in enumerate(s):
-            h += ord(c) * (BASE ** (n - i - 1))
-        return h % prime
+    def compare(str1, str2):
+        if len(str1) != len(str2): return False, 0
+        compares = 0
+        for i in range(len(str1)):
+            compares += 1
+            if str1[i] != str2[i]:
+                return False, compares
+        return True, compares
     
-    def rolling_hash(old_hash, oldc, newc, pattern_len):
-        new_hash = (BASE * (old_hash - ord(oldc) * BASE ** (pattern_len - 1)) + ord(newc)) % prime
-        return new_hash
+    def hash(old_hash, oldc):
+        return (old_hash + oldc) % prime
+    
+    def unhash(old_hash, oldc):
+        return (old_hash - oldc + prime) % prime
+
+    def hash_string(string):
+        hash_res = 0
+        for char in string:
+            hash_res = hash(hash_res, ord(char))
+        return hash_res
 
     if text == "" or pattern == "": return [], 0
 
     result = []
     n = len(text)
     m = len(pattern)
-    hp = hash(pattern)
-    htw = hash(text[:m])
+    hp = hash_string(pattern)
+    htw = hash_string(text[:m])
     compares = 0
 
-    if htw == hp and text[:m] == pattern:
-        compares += m
-        result.append(0)
-
-    for i in range(1, n - m + 1):
-        compares += 1
-        htw = rolling_hash(htw, text[i-1], text[m+i-1], m)
-
+    i = 0
+    while True:
         if htw == hp and text[i:i+m] == pattern:
-            compares += m
-            result.append(i)
+            comp_result, add_to_compares = compare(text[i:i+m], pattern)
+            compares += add_to_compares
+            if comp_result:
+                result.append(i)
+        
+        if n <= i + m:
+            break
 
+        htw = hash(htw, ord(text[i+m]))
+        htw = unhash(htw, ord(text[i]))
+        i += 1
     return result, compares

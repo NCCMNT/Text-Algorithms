6778106ea8c27ec2e662d37433437b8000e1bbd4
NCCMNT <chmielewski2003@gmail.com>
2025-05-25 00:03:25 +0200

    lab5 fin

--- lab5/suffix_array.py
+++ lab5/suffix_array.py
@@ -29,18 +29,43 @@ class SuffixArray:
             self.suffixes[i] = suff.index
 
     def find_pattern(self, pattern: str):
-        i = 0
-        n = len(self.suffixes)
-        m = len(pattern)
-        compares = 0
-        while i < n and self.suffix(self.suffixes[i]) < pattern:
-            i += 1
-            compares += m
 
-        j = i
-        while j < n and self.suffix(self.suffixes[j]).startswith(pattern):
-            compares += m
-            j += 1
+        def compare_strings(str1, str2):
+            compares = 0
+            min_len = min(len(str1), len(str2))
+
+            for i in range(min_len):
+                compares += 1
+                if str1[i] < str2[i]: return -1, compares
+                elif str1[i] > str2[i]: return 1, compares
+
+            if len(str1) < len(str2): return -1, compares + 1
+            elif len(str1) > len(str2): return 1, compares + 1
+            return 0, compares
+
+        def bsearch(T, val, side):
+            nonlocal key_fun
+            l, r = 0, len(T) - 1
+            compares = 0
+            while l <= r:
+                mid = (l + r) // 2
+                comp_result, add_to_comp = compare_strings(val, key_fun(T[mid]))
+                compares += add_to_comp
+                if comp_result == 0:
+                    if side == 0: r = mid - 1
+                    elif side == 1: l = mid + 1
+                elif comp_result == -1:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            return l, compares
         
-        if self.count_compares: return self.suffixes[i:j], compares
-        return self.suffixes[i:j]
+        if len(pattern) == 0:
+            return [], 0
+
+        m = len(pattern)
+        key_fun = lambda x: self.suffix(x)[:m]
+        index_l, comp1 = bsearch(self.suffixes, pattern, 0)
+        index_r, comp2 = bsearch(self.suffixes, pattern, 1)
+
+        return self.suffixes[index_l:index_r], comp1 + comp2 if self.count_compares else self.suffixes[index_l:index_r]
